b938ec2e7cdc83ec0471da9de1ba5b181610c339
luthfynur <nurluthfy98@gmail.com>
2021-05-25 15:31:18 +0700

    Add more palindrome test

--- palindrome/test.js
+++ palindrome/test.js
@@ -1,5 +1,13 @@
 const palindrome = require("./index");
 
-test("aba adalah palindrome", () => {
+test("'aba' is a palindrome", () => {
   expect(palindrome("aba")).toBeTruthy();
+});
+
+test("' aba' is not a palindrome", () => {
+  expect(palindrome(" aba")).toBeFalsy();
+});
+
+test("'kodok' is a palindrome", () => {
+  expect(palindrome("kodok")).toBeTruthy();
 });
