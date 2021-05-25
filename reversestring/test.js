816819e8fb6832e09e2c63736046048cbb101a1e
luthfynur <nurluthfy98@gmail.com>
2021-05-25 16:10:36 +0700

    add more testing

--- reversestring/test.js
+++ reversestring/test.js
@@ -1,13 +1,21 @@
-const reverseString = require("./index");
+const reverse = require("./index");
 
 test("Reverse reverseString a string", () => {
-  expect(reverseString("abcd")).toEqual("dcba");
+  expect(reverse.reverseString("abcd")).toEqual("dcba");
 });
 
 test("Reverse reverseString a string", () => {
-  expect(reverseString("  abcd")).toEqual("dcba  ");
+  expect(reverse.reverseString("  abcd")).toEqual("dcba  ");
 });
 
 test("Reverse reverseString a string", () => {
-  expect(reverseString("lutpi")).toEqual("iptul");
+  expect(reverse.reverseString("lutpi")).toEqual("iptul");
+});
+
+test("Reverse reverseStringTwo a string", () => {
+  expect(reverse.reverseStringTwo("nur")).toEqual("run");
+});
+
+test("Reverse reverseStringTwo a string", () => {
+  expect(reverse.reverseStringTwo("wahyudi")).toEqual("iduyhaw");
 });
