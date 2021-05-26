54db2301b79c79759f0d0318474d359aa0d1f88b
luthfynur <nurluthfy98@gmail.com>
2021-05-26 13:43:38 +0700

    Add comments

--- palindrome/index.js
+++ palindrome/index.js
@@ -1,3 +1,11 @@
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
 function palindrome(str) {
   for (let i = 0; i <= str.length; i++) {
     const compare = str[i] === str[str.length - i - 1];
