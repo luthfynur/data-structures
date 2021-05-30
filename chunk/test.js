b23b287094ecb6bade20b5d838448996dbc68ea6
luthfynur <nurluthfy98@gmail.com>
2021-05-30 22:14:10 +0700

    Fix some typos

--- chunk/test.js
+++ chunk/test.js
@@ -13,7 +13,7 @@ test("chunk divides an array of 10 elements with chunk size 2", () => {
   ]);
 });
 
-test("chunk divides an array of 10 elements with chunkTwo size 2", () => {
+test("chunkTwo divides an array of 10 elements with chunk size 2", () => {
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const chunked = chunk.chunkTwo(arr, 2);
 
@@ -33,7 +33,7 @@ test("chunk divides an array of 3 elements with chunk size 1", () => {
   expect(chunked).toEqual([[1], [2], [3]]);
 });
 
-test("chunk divides an array of 3 elements with chunkTwo size 1", () => {
+test("chunkTwo divides an array of 3 elements with chunk size 1", () => {
   const arr = [1, 2, 3];
   const chunked = chunk.chunkTwo(arr, 1);
 
@@ -50,7 +50,7 @@ test("chunk divides an array of 5 elements with chunk size 3", () => {
   ]);
 });
 
-test("chunk divides an array of 5 elements with chunkTwo size 3", () => {
+test("chunkTwo divides an array of 5 elements with chunk size 3", () => {
   const arr = [1, 2, 3, 4, 5];
   const chunked = chunk.chunkTwo(arr, 3);
 
@@ -71,7 +71,7 @@ test("chunk divides an array of 13 elements with chunk size 5", () => {
   ]);
 });
 
-test("chunk divides an array of 13 elements with chunkTwo size 5", () => {
+test("chunkTwo divides an array of 13 elements with chunk size 5", () => {
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
   const chunked = chunk.chunkTwo(arr, 5);
 
